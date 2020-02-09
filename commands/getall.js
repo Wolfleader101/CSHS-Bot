0366edc4ab026d164f8e68dd1dd0c0cd443a1bc6
Wolfleader <james.morgan@iinet.net.au>
2020-02-09 17:09:55 +0800

    Added GetAll command
    Added a command and functionality to show people how to get all members names in the guild

---
+++ commands/getall.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: 'getall',
+    execute(message, args) {
+        let res = message.guild.members;
+
+        // array to store users
+        let Users = [];
+
+        res.forEach(el => {
+            let username = el.user.username;
+            let tag = `#${el.user.discriminator}`;
+
+
+            // Use this variable for getting the user in a format like WOLFLEADER#0999
+            let User = username + tag;
+
+            // add each user to the array when looping
+            Users.push(User);
+
+           
+        });
+         //example of what to do with the users
+         message.channel.send(Users);
+    },
+};
