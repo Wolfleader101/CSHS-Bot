e4949e920025d06fb148a6e398ba24eb526203e2
Wolfleader <james.morgan@iinet.net.au>
2020-02-10 17:01:10 +0800

    use chalk instead

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,11 @@
 const {client} = require('../app')
+const chalk = require('chalk');
 
 // The startup event
 client.on('ready', () => {
     console.log('\n================================\n');
-    console.log('   --', `\x1b[31m`, `\x1b[4m`,' CSHS Bot Working ', `\x1b[0m`,' --');
-    console.log('   --  Status:',`\x1b[32m`,'Online  ', `\x1b[0m`,'   --\n');
+    console.log('   --', chalk.red(' CSHS Bot Working  '), '   --');
+    console.log('   --  Status:',chalk.green('Online   '), '    --\n');
     console.log('================================');
     client.user.setActivity('==help for commands', { type: 'STREAMING', url: 'https://www.twitch.tv/shroud'})
   
