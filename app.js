c72daa63596a452c8dfbf593d242133c915176ef
Wolfleader <james.morgan@iinet.net.au>
2020-02-10 17:06:37 +0800

    add message event

--- app.js
+++ app.js
@@ -11,34 +11,12 @@ const ddiff = require('return-deep-diff');
 const broadcast = client.voice.createBroadcast();
 const ytdl = require('ytdl-core');
 
-module.exports = {
-  client: client
-};
-
 // Handlers
+//export client for events
+module.exports = {client: client};
+// import events
 require("./handlers")(client);
 
-//commands
-client.on('message', message => {
-
-//stop spam
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-  //allow for arguments.
-  const args = message.content.slice(prefix.length).split(/ +/);
-  const command = args.shift().toLowerCase();
-
-  if (!client.commands.has(command)) return;
-
-  try {
-      client.commands.get(command).execute(message, args);
-  }
-  catch (error) {
-      console.error(error);
-      message.reply('there was an error trying to execute that command!');
-  }
-
-});
 
 //catch any errors
 process.on('unhandledRejection', err => console.error(`Uncaught Promise Error: \n${err.stack}`));
