e5896ac6b190db40dc00f09e1d6d0e9c072670b6
Wolfleader <james.morgan@iinet.net.au>
2020-02-15 14:13:42 +0800

    send to local server

--- commands/getall.js
+++ commands/getall.js
@@ -3,38 +3,38 @@ const fetch = require('node-fetch');
 module.exports = {
     name: 'getall',
     execute(message, args) {
-        let res = message.guild.members;
+            let res = message.guild.members;
 
-        // array to store users
-        let Users = [];
-
-        res.forEach(el => {
-            let username = el.user.username;
-            let tag = `#${el.user.discriminator}`;
-
-
-            // Use this variable for getting the user in a format like WOLFLEADER#0999
-            let User = username + tag;
-
-            // add each user to the array when looping
-            Users.push(User);
-
-
-        });
-        
-        //example of what to do with the users
-        message.channel.send(Users);
-
-        const data = {Users};
-        // replace this with your own URL
-        fetch('https://httpbin.org/post', {
-                method: 'post',
-                body: JSON.stringify(data),
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            })
-            .then(res => console.log(res))
-            // .then(json => console.log(json));
-    },
+            // array to store users
+            let Users = [];
+    
+            res.forEach(el => {
+                let username = el.user.username;
+                let tag = `#${el.user.discriminator}`;
+    
+    
+                // Use this variable for getting the user in a format like WOLFLEADER#0999
+                let User = username + tag;
+    
+                // add each user to the array when looping
+                Users.push(User);
+    
+    
+            });
+            
+            //example of what to do with the users
+            message.channel.send(Users);
+    
+            const data = {Users};
+            // replace this with your own URL
+            fetch('http://localhost:3302/PostUser', {
+                    method: 'post',
+                    body: JSON.stringify(data),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                })
+                .then(res => console.log(res))
+                // .then(json => console.log(json));
+    }
 };
